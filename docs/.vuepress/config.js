86bf6af8e0aac54d87a733a52326896264e94abf
hrf-666 <3398775460@qq.com>
2023-11-21 15:03:23 +0800

    添加默认主题

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,41 @@
+// .vuepress/config.js
+module.exports = {
+    base: '/',
+    themeConfig: {
+        logo: '/assets/img/logo.png',
+        nav: [
+            {text: 'Home', link: '/'},
+            {text: 'Guide', link: '/guide/'},
+            {text: '源码', link: 'https://gitee.com/huang-ruifang/vuepress-blog'},
+            {text: 'Guide', link: '/guide/', target: '_blank'},
+            {
+                text: 'Languages',
+                ariaLabel: 'Language Menu',
+                items: [
+                    {text: 'Chinese', link: '/language/chinese/'},
+                    {text: 'Japanese', link: '/language/japanese/'}
+                ]
+            },
+            {
+                text: 'Languages',
+                items: [
+                    {
+                        text: 'Group1',
+                        items: [
+                            {text: 'Chinese', link: '/language/chinese/'},
+                            {text: 'Japanese', link: '/language/japanese/'}
+                        ]
+                    },
+                    {text: 'Group2', items: [/*  */]}
+                ]
+            }
+        ],
+
+        sidebar: [
+            '/',
+            '/page-a',
+            ['/page-b', 'Explicit link text']
+        ]
+
+    }
+}
