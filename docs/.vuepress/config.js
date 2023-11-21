c4a775bddb7006b6b5282920fc051fa420350de1
hrf-666 <3398775460@qq.com>
2023-11-21 15:20:47 +0800

    添加默认主题

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,37 +5,37 @@ module.exports = {
         logo: '/assets/img/logo.png',
         nav: [
             {text: 'Home', link: '/'},
-            {text: 'Guide', link: '/guide/'},
+            // {text: 'Guide', link: '/guide/'},
             {text: '源码', link: 'https://gitee.com/huang-ruifang/vuepress-blog'},
-            {text: 'Guide', link: '/guide/', target: '_blank'},
-            {
-                text: 'Languages',
-                ariaLabel: 'Language Menu',
-                items: [
-                    {text: 'Chinese', link: '/language/chinese/'},
-                    {text: 'Japanese', link: '/language/japanese/'}
-                ]
-            },
-            {
-                text: 'Languages',
-                items: [
-                    {
-                        text: 'Group1',
-                        items: [
-                            {text: 'Chinese', link: '/language/chinese/'},
-                            {text: 'Japanese', link: '/language/japanese/'}
-                        ]
-                    },
-                    {text: 'Group2', items: [/*  */]}
-                ]
-            }
+            // {text: 'Guide', link: '/guide/', target: '_blank'},
+            // {
+            //     text: 'Languages',
+            //     ariaLabel: 'Language Menu',
+            //     items: [
+            //         {text: 'Chinese', link: '/language/chinese/'},
+            //         {text: 'Japanese', link: '/language/japanese/'}
+            //     ]
+            // },
+            // {
+            //     text: 'Languages',
+            //     items: [
+            //         {
+            //             text: 'Group1',
+            //             items: [
+            //                 {text: 'Chinese', link: '/language/chinese/'},
+            //                 {text: 'Japanese', link: '/language/japanese/'}
+            //             ]
+            //         },
+            //         {text: 'Group2', items: [/*  */]}
+            //     ]
+            // }
         ],
 
-        sidebar: [
-            '/',
-            '/page-a',
-            ['/page-b', 'Explicit link text']
-        ]
+        // sidebar: [
+        //     '/',
+        //     '/page-a',
+        //     ['/page-b', 'Explicit link text']
+        // ]
 
     }
 }
