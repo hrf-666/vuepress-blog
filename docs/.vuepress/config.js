ca497da42a09f782200bd2c82a592a1423bc8a69
hrf-666 <3398775460@qq.com>
2023-12-25 18:41:19 +0800

    修改

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -34,23 +34,23 @@ module.exports = {
         searchMaxSuggestions: 10,
         permalink: "/:year/:month/:day/:slug",
         smoothScroll: true,//页面滚动效果
-        subSildebar: 'auto', // 或者生成二级菜单
-        // 假定是 GitHub. 同时也可以是一个完整的 GitLab URL
-        repo: 'https://gitee.com/huang-ruifang/vuepress-blog',
-        // 自定义仓库链接文字。默认从 `themeConfig.repo` 中自动推断为
-        // "GitHub"/"GitLab"/"Bitbucket" 其中之一，或是 "Source"。
-        repoLabel: '查看源码',
-        // 以下为可选的编辑链接选项
-        // 假如你的文档仓库和项目本身不在一个仓库：
-        // docsRepo: 'vuejs/vuepress',
-        // 假如文档不是放在仓库的根目录下：
-        docsDir: 'docs',
-        // 假如文档放在一个特定的分支下：
-        docsBranch: 'master',
-        // 默认是 false, 设置为 true 来启用
-        editLinks: true,
-        // 默认为 "Edit this page"
-        editLinkText: '帮助我们改善此页面！',
+        subSidebar: 'auto', // 或者生成二级菜单
+        // // 假定是 GitHub. 同时也可以是一个完整的 GitLab URL
+        // repo: 'https://gitee.com/huang-ruifang/vuepress-blog',
+        // // 自定义仓库链接文字。默认从 `themeConfig.repo` 中自动推断为
+        // // "GitHub"/"GitLab"/"Bitbucket" 其中之一，或是 "Source"。
+        // repoLabel: '查看源码',
+        // // 以下为可选的编辑链接选项
+        // // 假如你的文档仓库和项目本身不在一个仓库：
+        // // docsRepo: 'vuejs/vuepress',
+        // // 假如文档不是放在仓库的根目录下：
+        // docsDir: 'docs',
+        // // 假如文档放在一个特定的分支下：
+        // docsBranch: 'master',
+        // // 默认是 false, 设置为 true 来启用
+        // editLinks: true,
+        // // 默认为 "Edit this page"
+        // editLinkText: '帮助我们改善此页面！',
         nav: navConfig,
         sidebar: sidebarConfig,
     }
