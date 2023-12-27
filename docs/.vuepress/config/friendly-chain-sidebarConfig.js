fd1447b471e2998cab9c25758d0222dfd617cc7b
hrf-666 <3398775460@qq.com>
2023-12-27 09:35:39 +0800

    修改

---
+++ docs/.vuepress/config/friendly-chain-sidebarConfig.js
@@ -0,0 +1,11 @@
+const friendly_chain_sidebarConfig = [
+    {
+        title: '后端',   // 必要的
+        path: '/friendly-chain/',      // 可选的, 标题的跳转链接，应为绝对路径且必须存在
+        initialOpenGroupIndex: -1, // 可选的, 默认值是 0
+        collapsable: false, // 可选的, 默认值是 true,
+        sidebarDepth: -1,    // 可选的, 默认值是 1
+    },
+
+]
+exports.friendly_chain_sidebarConfig = friendly_chain_sidebarConfig;
