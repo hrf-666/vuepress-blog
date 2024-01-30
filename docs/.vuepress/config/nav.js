3f3f4592fdb37afcbbfae7329891e6e58a994a7f
hrf-666 <3398775460@qq.com>
2024-01-30 14:34:14 +0800

    修改

--- docs/.vuepress/config/nav.js
+++ docs/.vuepress/config/nav.js
@@ -1,31 +1,32 @@
 const navConfig = [
-        {text: '首页', link: '/'},
-        // {text: '程序员导航', link: 'https://hrf-666.github.io/navigation', target: '_blank'},
-        {text: '程序员导航', link: '/navigation/',},
-        {
-            text: '面试',
-            link: '/study-notes/'
-        },
-        {
-            text: '博文',
-            link: '/blog-article/'
-        },
-        {
-            text: '代码Demo',
-            link: '/code-demo/',
-        },
-        {
-            text: '友链',
-            link: '/friendly-chain/'
-        },
-        {
-            text: '其他',
-            link: '/other/'
-        },
-        {
-            text: '阅读',
-            link: '/read/'
-        },
-    ]
+    {text: '首页', link: '/'},
+    // {text: '程序员导航', link: 'https://hrf-666.github.io/navigation', target: '_blank'},
+    {text: '程序员导航', link: '/navigation/',},
+    {
+        text: '面试',
+        link: '/study-notes/'
+    },
+    {
+        text: '博文',
+        link: '/blog-article/'
+    },
+    {
+        text: '代码Demo',
+        link: '/code-demo/',
+    },
+    {
+        text: '友链',
+        link: '/friendly-chain/'
+    },
+    {
+        text: '阅读',
+        link: '/read/'
+    },
+    {
+        text: '其他',
+        link: '/other/'
+    },
+
+]
 
 exports.navConfig = navConfig;
