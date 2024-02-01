2f24c874a49170518f93a564c4aa8400ce619e26
huang-rui <3398775460@qq.com>
2024-02-01 22:39:57 +0800

    修改

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,22 +2,12 @@ const {sidebarConfig} = require("./config/sidebar");
 const {navConfig} = require("./config/nav");
 const {pluginsConfig} = require("./config/plugins");
 const {markdownConfig} = require("./config/markdownConfig");
+const {headConfig} = require("./config/headConfig")
 
 // console.log(sidebarConfig)
 module.exports = {
     title: 'Rui',
-    head: [
-        ['link', {rel: 'icon', href: '/assets/img/logo.png'}],
-        ["meta", {name: "referrer", content: "no-referrer"}],
-        ['link', {rel: 'manifest', href: '/manifest.json'}],
-        ['meta', { name: 'theme-color', content: '#3eaf7c' }],
-        ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
-        ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
-        ['link', { rel: 'apple-touch-icon', href: '/icons/apple-touch-icon-152x152.png' }],
-        ['link', { rel: 'mask-icon', href: '/icons/safari-pinned-tab.svg', color: '#3eaf7c' }],
-        ['meta', { name: 'msapplication-TileImage', content: '/icons/msapplication-icon-144x144.png' }],
-        ['meta', { name: 'msapplication-TileColor', content: '#000000' }]
-    ],
+    head: headConfig,
     description: '点点滴滴',
     base: '/vuepress-blog/',
     devServer: {
