50f998a77d4617b93c11e5df46942a7fe330a89b
HRF <3398775460@qq.com>
2024-06-07 20:58:44 +0800

    添加自动部署文件

--- docs/blog-article/database/redis/redis7-study/src/main/java/com/luojia/redis7_study/mapper/CustomerMapper.java
+++
@@ -1,7 +0,0 @@
-package com.luojia.redis7_study.mapper;
-
-import com.luojia.redis7_study.entities.Customer;
-import tk.mybatis.mapper.common.Mapper;
-
-public interface CustomerMapper extends Mapper<Customer> {
-}
