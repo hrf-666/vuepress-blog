fa5fabc1012055c9be3254c7ce74715a3901dd87
hrf-666 <3398775460@qq.com>
2024-07-23 16:44:05 +0800

    修改

--- docs/.vuepress/config/blog-article-sidebarConfig.js
+++ docs/.vuepress/config/blog-article-sidebarConfig.js
@@ -3,9 +3,9 @@ const blog_article_sidebarConfig = [
     {
         title: 'Java',   // 必要的
         // path: '/blog-article/java/',      // 可选的, 标题的跳转链接，应为绝对路径且必须存在
-        initialOpenGroupIndex: -1, // 可选的, 默认值是 0
-        collapsable: false, // 可选的, 默认值是 true,
-        sidebarDepth: -1,    // 可选的, 默认值是 1
+        // initialOpenGroupIndex: -1, // 可选的, 默认值是 0
+        // collapsable: false, // 可选的, 默认值是 true,
+        // sidebarDepth: -1,    // 可选的, 默认值是 1
         children: [
             {
                 title: '软件安装',   // 必要的
